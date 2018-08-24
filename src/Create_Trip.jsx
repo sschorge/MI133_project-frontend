10e6e3ed619938bf32ca56da91c30ae3f76be73e
Administrator <root>
2018-08-24 23:39:24 +0200

    added google maps implementation

--- src/Create_Trip.jsx
+++ src/Create_Trip.jsx
@@ -20,9 +20,74 @@ class CreateTrip extends React.Component {
                 <input type="text"/>
                 <input type="button" value="+"/>
             </div>
+			
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAd2nDFBZR6bqEuDnpxNzGM08Xlh1RTkWc&callback=initMap" async defer></script>
+            <div id="map">
+                {
+					var map;
+					var curMarker;
+					var geocoder, addressString = "";
 
-            <div>
-                {/* Div for Google Maps Code */}
+					function getLocationByLatLng(geocoder, latLng) {
+						var needsRetry = true;
+						
+						while (needsRetry) {
+							needsRetry = false;
+						
+							geocoder.geocode({
+								'latLng': latLng
+							}, 
+							function (results, status) {
+								switch (status) {
+									case google.maps.GeocoderStatus.OK:
+										if (results[0]) {
+											addressString = results[0].formatted_address;
+											console.log(addressString);
+										}
+										else {
+											console.log('No place found for latlng: ' + latLng);
+											addressString = "";
+										}
+										break;
+										
+									case google.maps.GeocoderStatus.OVER_QUERY_LIMIT: 
+										needsRetry = true;
+										
+									default:
+										addressString = "";
+										console.log('Geocoder failed due to: ' + status);
+										break;
+								}
+							})
+						}
+					}
+					
+					function onMapClick(event) {
+						var latLng = event.latLng;
+
+						if (curMarker != null) 
+							curMarker.setMap(null);
+						
+						curMarker = new google.maps.Marker({
+							position: latLng,
+							label: 'Trip',
+							map: map
+						});
+
+						getLocationByLatLng(geocoder, latLng);
+					}
+
+					function initMap() {
+					  map = new google.maps.Map(document.getElementById('map'), {
+							center: {lat: 54.32329270, lng: 10.12276520}, // Kiel
+							zoom: 10
+						});
+						
+						geocoder = new google.maps.Geocoder();
+						
+						google.maps.event.addListener(map, 'click', onMapClick);
+					}
+				}
             </div>
 
             <div>
@@ -42,9 +107,7 @@ class CreateTrip extends React.Component {
             </div>
         </form>
           </div>
-
     }
-
 }
 
 function mapStateToProps(state){
