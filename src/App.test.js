8221ac8c74d1404a0997d1fb42a785086f691837
simonarp <s_arp@gmx.de>
2018-08-31 03:27:08 +0200

    first test implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as actions from './actions'
+import reducer from './reducer';
+import initialState from './reducer';
+import {requestLogin} from './actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('the app logic', () => {
+	it('should return the initial state', () => {
+		expect(reducer(undefined, {})).toEqual(initialState());
+	});
+
+	it('should reset the state (RESET_STATE action)', () => {
+		expect(reducer(undefined, actions.reset_state())).toEqual(initialState());
+	});
+	
+	it('should set the regisration', () => {
+		const newState = reducer(undefined, actions.set_registration())
+		expect(newState).toHaveProperty('registration')
+	});
+	
+	it('should set the login', () => {
+		const expectedAction = {
+		  type: actions.SET_LOGIN,
+		  payload: "username"
+		}
+		const data = {message: "Congrats username! You logged in successfully!"}
+		const daten = {username: "username", password: "password"}
+		
+		expect(actions.set_login(data, daten)).toEqual(expectedAction)
+	});
+	
+	it('should hide the menu', () => {
+		const newState = reducer(undefined, actions.set_hide_menue())
+		expect(newState.hide_menue).toEqual(true)
+	});
+	
+	it('should set the logout', () => {
+		const newState = reducer(undefined, actions.set_logout())
+		expect(newState.login).toEqual(false)
+	});	
+	
+	it('should request register', () => {
+		requestRegister('test', 'test', 'Test', 'Test')
+	});
+
+	it('should log in', () => {
+		requestLogin('test', 'test')
+	});
 });
