3ecf26fbef4ee452f91c619c5be4a09debb238e5
sschorge <sven.schorge@student.fh-kiel.de>
2018-08-31 10:43:18 +0200

    added styling to langing page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React,{Component} from 'react';
+import React, {Component } from 'react';
 import Trips from './Trips'
 import './App.css';
 import { connect } from 'react-redux';
 import Register from './Register';
 import Login from './Login';
 class App extends Component {
-
   render() {
-    const {user} = this.props;
-    const {registration} = this.props;
+    const { user } = this.props;
+    const { registration } = this.props;
     return (
       <div className="App">
-      <h2> Water Sports Lookbook</h2>
-      <Login/>
-      <Register/>
-      {!registration ?
-        <div>
-          <h4>Welcome {user}</h4> 
-          <Trips/>
-          </div>:
-        false
-      }
+        <h2> Water Sports Lookbook</h2>
+        <div class="row">
+          <div class="col-4"> </div>
+          {!registration ?
+            <div class="col-4">
+              <h4>Welcome {user}</h4>
+              <Trips />
+            </div> :
+            false
+          }
+          <div class="col-4" >
+            <Login />
+            <Register />
+          </div>
+        </div>
       </div>
     );
   }
 }
-function mapStateToProps(state){
+
+function mapStateToProps(state) {
   return {
     user: state.user,
     registration: state.registration,
