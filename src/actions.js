818e06de428aa76d5232566b0cd6695b5aaa4de6
sschorge <sven.schorge@student.fh-kiel.de>
2018-09-06 14:18:19 +0200

    added function joinTrip to send data to database for joining a trip

--- src/actions.js
+++ src/actions.js
@@ -14,16 +14,16 @@ export const SET_HIDE_MENUE = 'SET_HIDE_MENUE'
 export const set_hide_menue = () => ({ type: SET_HIDE_MENUE })
 
 export const SET_CREATE_TRIP = 'SET_CREATE_TRIP'
-export const set_create_trip = (create_trip,hide_menue) => ({type: SET_CREATE_TRIP, payload: create_trip, hide_menue})
+export const set_create_trip = (create_trip, hide_menue) => ({ type: SET_CREATE_TRIP, payload: create_trip, hide_menue })
 
 export const SET_SIGN_TRIP = 'SET_SIGN_TRIP'
-export const set_sign_trip = (sign_trip, hide_menue) => ({type: SET_SIGN_TRIP, payload: sign_trip, hide_menue})
+export const set_sign_trip = (sign_trip, hide_menue) => ({ type: SET_SIGN_TRIP, payload: sign_trip, hide_menue })
 
 export const SET_LOGIN = 'LOGIN'
 export const set_login = (data, daten) => {
     let user_id = data.user.id;
     let username = daten.username;
-    return { type: SET_LOGIN, payload: {username, user_id}}
+    return { type: SET_LOGIN, payload: { username, user_id } }
 }
 
 export const requestLogin = (username, password) => async dispatch => {
@@ -78,20 +78,40 @@ export const createTrip = (boat_id, crew, latitude, longitude, departure, arriva
         }).then(res => res.json())
             .then(data => {
                 console.log(data)
-                if(data.success){
+                if (data.success) {
                     alert("Create Trip was Successful!")
                 } else {
                     alert("Create Trip failed!")
                 }
-                dispatch(set_create_trip({bool: !data.success,hide_menue: false}))
+                dispatch(set_create_trip({ bool: !data.success, hide_menue: false }))
             })
     } catch (e) {
         console.error(e)
     }
 }
 
-export const joinTrip = (userid, tripid) => async dispatch =>{
-
+export const joinTrip = (userid, tripid) => async dispatch => {
+    console.log("joinTrip funcsion " + userid + " " + tripid)
+    try {
+        let url = "http://rcpoonkk8vbqkyiw.myfritz.net:3000/join_trip";
+        let member_id = userid
+        let trip_id = tripid
+        let daten = {trip_id,member_id };
+        console.log(daten)
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(daten),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "same-origin"
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    } catch (e) {
+        console.error(e)
+    }
 }
 
 
